1a360331411cc6e64d22a280d2a8666f808986f1
andrewpolar <61638749+andrewpolar@users.noreply.github.com>
2024-07-03 23:08:50 -0400

    Update DataHolder.cpp

--- KANC++/DataHolder.cpp
+++ KANC++/DataHolder.cpp
@@ -3,6 +3,63 @@
 #include <ctime>
 #include <cstdlib> 
 
+Formula3::~Formula3() {
+    if (NULL != target) delete target;
+    if (NULL != inputs) {
+        for (int i = 0; i < _N; ++i) {
+            if (NULL != inputs[i]) {
+                delete inputs[i];
+            }
+        }
+        delete[] inputs;
+    }
+}
+
+double Formula3::Function(double* input) {
+    //y = (1/pi)*(2+2*x3)*(1/3)*(atan(20*exp(x5)*(x1-0.5+x2/6))+pi/2) + (1/pi)*(2+2*x4)*(1/3)*(atan(20*exp(x5)*(x1-0.5-x2/6))+pi/2);
+    double pi = 3.14159265359;
+    double y = (1.0 / pi);
+    y *= (2.0 + 2.0 * input[2]);
+    y *= (1.0 / 3.0);
+    y *= atan(20.0 * atan(input[4]) * (input[0] - 0.5 + input[1] / 6.0)) + pi / 2.0;
+
+    double z = (1.0 / pi);
+    z *= (2.0 + 2.0 * input[3]);
+    z *= (1.0 / 3.0);
+    z *= atan(20.0 * atan(input[4]) * (input[0] - 0.5 - input[1] / 6.0)) + pi / 2.0;
+
+    return y + z;
+}
+
+void Formula3::GetInput(double* input) {
+    input[0] = (rand() % 100) / 100.0;
+    input[1] = (rand() % 100) / 100.0;
+    input[2] = (rand() % 100) / 100.0;
+    input[3] = (rand() % 100) / 100.0;
+    input[4] = (rand() % 100) / 100.0;
+}
+
+double Formula3::GetTarget(double* input) {
+    return Function(input);
+}
+
+void Formula3::GenerateData(int N) {
+    _N = N;
+    inputs = new double* [N];
+    for (int i = 0; i < N; ++i) {
+        inputs[i] = new double[nInputs];
+    }
+    target = new double[N];
+
+    int counter = 0;
+    while (true) {
+        GetInput(inputs[counter]);
+        target[counter] = Function(inputs[counter]);
+        if (++counter >= N) break;
+    }
+}
+
+///////////////////////
 Formula4::~Formula4() {
     if (NULL != target) delete target;
     if (NULL != inputs) {
